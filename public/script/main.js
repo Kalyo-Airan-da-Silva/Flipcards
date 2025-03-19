a3e5b61307c26644c9167f47e6b5621dd53cc35e
Kalyo Airan da Silva <kalyosilva@gmail.com>
2025-03-19 10:33:52 -0300

    Reestruturação do Front-end

--- public/script/main.js
+++ public/script/main.js
@@ -4,10 +4,14 @@ function flip(){
     if (!flipped){
         document.getElementById("flashcard").style = "transform : rotateY(180deg)";
         document.getElementById("Content").style = "transform : rotateY(180deg)";
+        document.getElementById("side").style = "transform : rotateY(180deg)";
+        document.getElementById("Questao").innerText = "Resposta";
     }
     else{
         document.getElementById("flashcard").style = "";
         document.getElementById("Content").style = "";
+        document.getElementById("side").style = "";
+        document.getElementById("Questao").innerText = "Questão";
     }
 
     flipped = !flipped;
